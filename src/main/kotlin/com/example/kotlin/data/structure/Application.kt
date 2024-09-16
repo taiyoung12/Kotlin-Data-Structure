cdcd72ae226088a1a2031f57b4fd8ff471759db4
taiyoung <qq3222@naver.com>
2024-09-16 16:57:55 +0900

    세팅

---
+++ src/main/kotlin/com/example/kotlin/data/structure/Application.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin.data.structure
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlin/data/structure/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin.data.structure
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

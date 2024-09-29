b734fac7e77e3bdc57deaac1decab7431b36a3ee
taiyoung <qq3222@naver.com>
2024-09-29 11:13:08 +0900

    트리 초기화

--- src/main/kotlin/com/example/kotlin/data/structure/Tree/CustomTree.kt
+++ src/main/kotlin/com/example/kotlin/data/structure/Tree/CustomTree.kt
@@ -1,20 +1,5 @@
 package com.example.kotlin.data.structure.Tree
 
 class CustomTree {
-    private val _nodes = mutableListOf<CustomTreeNode>()
-    val nodes: List<CustomTreeNode> = _nodes
-    val size: Int
-        get() = _nodes.size
 
-    val root: CustomTreeNode?
-        get() = _nodes.firstOrNull()
-
-    fun addNode(node: CustomTreeNode) {
-        _nodes.add(node)
-    }
-
-    fun addNode(node: CustomTreeNode, leftNode: CustomTreeNode, rightNode: CustomTreeNode) {
-        node.addLeft(leftNode)
-        node.addRight(rightNode)
-    }
 }
--- src/main/kotlin/com/example/kotlin/data/structure/Tree/CustomTreeNode.kt
+++ src/main/kotlin/com/example/kotlin/data/structure/Tree/CustomTreeNode.kt
@@ -4,11 +4,4 @@ data class CustomTreeNode(
     val value: Int = 0,
     var left: CustomTreeNode? = null,
     var right: CustomTreeNode? = null
-){
-    fun addLeft(node: CustomTreeNode){
-        left = node
-    }
-    fun addRight(node: CustomTreeNode){
-        right = node
-    }
-}
+)
